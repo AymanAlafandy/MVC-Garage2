11bd2950aad8fa818ca9c44c665aba82e7ef08cf
ayman alafandy <a.alafandy@outlook.sa>
2017-12-05 11:09:11 +0100

    Add Model class

---
+++ Garage-WebApp/Garage-WebApp/Models/ParkedVehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage_WebApp.Models
+{
+    public class ParkedVehicle
+    {
+        public string Type { get; set; }
+        public int RegistrationNumber { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int NumberOfWheels { get; set; }
+    }
+}
