723d71b4c672334ee48af6a6c93b1575689dbc5b
ayman alafandy <a.alafandy@outlook.sa>
2017-12-05 12:03:13 +0100

    Ayman002

---
+++ Garage-WebApp/Garage-WebApp/Controllers/vehicleController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage_WebApp.Controllers
+{
+    public class vehicleController
+    {
+        // Vehicles should be able to park and Check out. "unpark"
+        //when  a user parks, they should be able to register all information about the vehicle.
+        //There should be an OVERVIEW-VIEW where all vehicles currently parked are displayes, but only with some basic data like : RegNr  Type, Color, Parking time*.
+        //There should be a DETAIL-VIEW where you can see all information about a specific vehicle.
+    }
+}
